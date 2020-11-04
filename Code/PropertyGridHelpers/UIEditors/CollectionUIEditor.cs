5d8ff420eb0b069a6974b0feeb15243e94bec672
David Parvin <parvin_david@hotmail.com>
2020-11-04 12:02:33 -0700

    Added a generic collections editor

---
+++ Code/PropertyGridHelpers/UIEditors/CollectionUIEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+namespace PropertyGridHelpers.UIEditors
+{
+    /// <summary>
+    /// Generic class for dealing with a list of items of a specific type
+    /// </summary>
+    /// <typeparam name="T">type of item in the list</typeparam>
+    /// <seealso cref="System.ComponentModel.Design.CollectionEditor" />
+    /// <seealso cref="CollectionEditor" />
+    public class CollectionUIEditor<T> : CollectionEditor where T : new()
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CollectionUIEditor&lt;T&gt;" /> class.
+        /// </summary>
+        public CollectionUIEditor() : base(type: typeof(List<T>))
+        {
+
+        }
+
+        /// <summary>
+        /// Creates the instance.
+        /// </summary>
+        /// <param name="itemType">Type of the item.</param>
+        /// <returns></returns>
+        protected override object CreateInstance(Type itemType)
+        {
+            return new T();
+        }
+    }
+}
--- Code/SampleControls/TestControl.cs
+++ Code/SampleControls/TestControl.cs
@@ -1,5 +1,6 @@
 ﻿using PropertyGridHelpers.Converters;
 using PropertyGridHelpers.UIEditors;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing.Design;
 using System.Windows.Forms;
@@ -30,5 +31,29 @@ namespace SampleControls
         [Editor(typeof(FlagEnumUIEditor<EnumTextConverter<TestEnums>>), typeof(UITypeEditor))]
         [TypeConverter(typeof(EnumTextConverter<TestEnums>))]
         public TestEnums Test { get; set; }
+
+        ///// <summary>
+        ///// Gets or sets the strings.
+        ///// </summary>
+        ///// <value>
+        ///// The strings.
+        ///// </value>
+        ///// <summary>
+        ///// Gets or sets the test.
+        ///// </summary>
+        ///// <value>
+        ///// The test.
+        ///// </value>
+        ///// <remarks>
+        ///// The <see cref="EditorAttribute"/> is used to setup the dropdown on the grid to display the data the way the
+        ///// programmer intends the Enum to be represented.  The <see cref="TypeConverterAttribute"/>
+        ///// is used to set the text in the grid in a normal basis.
+        ///// </remarks>
+        //[Category("Test Items")]
+        //[Description("A test of processing a list of strings")]
+        //[DisplayName("Strings")]
+        //[Editor(typeof(PropertyGridHelpers.UIEditors.CollectionUIEditor<string>), typeof(UITypeEditor))]
+        //[TypeConverter(typeof(EnumTextConverter<TestEnums>))]
+        //public List<string> Strings { get; }
     }
 }
