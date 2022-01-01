e72728db65c6d3a141b5356c2c34539a2191c609
David Parvin <parvin_david@hotmail.com>
2021-12-31 18:34:29 -0700

    Trying to get unit tests to all run

---
+++ Code/PropertyGridHelpersTest/GlobalSuppressions.cs
@@ -0,0 +1,32 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+#if NET35
+[assembly: SuppressMessage("Assertions", "xUnit2006:Do not use invalid string equality check", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net35.Controls.FlagCheckedListBoxItemTest.CaptionSetCorrectlyTest")]
+[assembly: SuppressMessage("Assertions", "xUnit2006:Do not use invalid string equality check", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net35.Controls.FlagCheckedListBoxTest.AddItemCaptionSetCorrectlyTest")]
+[assembly: SuppressMessage("Assertions", "xUnit2006:Do not use invalid string equality check", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net35.Controls.FlagCheckedListBoxTest.AddValuesCaptionSetCorrectlyTest")]
+#endif
+
+#if NET5_0
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net50.Controls.FlagCheckedListBoxTest.AddItemCaptionSetCorrectlyTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net50.Controls.FlagCheckedListBoxTest.AddValuesCaptionSetCorrectlyTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net50.Controls.FlagCheckedListBoxTest.AddItemValueSetCorrectlyTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net50.Controls.FlagCheckedListBoxTest.AddValuesValueSetCorrectlyTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net50.Controls.FlagCheckedListBoxTest.ItemCheckedTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net50.Controls.FlagCheckedListBoxTest.SetEnumValueTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net50.Controls.FlagCheckedListBoxTest.GetEnumValueTest(System.Int32)")]
+#endif
+
+#if NET6_0
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net60.Controls.FlagCheckedListBoxTest.AddItemCaptionSetCorrectlyTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net60.Controls.FlagCheckedListBoxTest.AddValuesCaptionSetCorrectlyTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net60.Controls.FlagCheckedListBoxTest.AddItemValueSetCorrectlyTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net60.Controls.FlagCheckedListBoxTest.AddValuesValueSetCorrectlyTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net60.Controls.FlagCheckedListBoxTest.ItemCheckedTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net60.Controls.FlagCheckedListBoxTest.SetEnumValueTest")]
+[assembly: SuppressMessage("Style", "IDE0063:Use simple 'using' statement", Justification = "<Pending>", Scope = "member", Target = "~M:PropertyGridHelpersTest.net60.Controls.FlagCheckedListBoxTest.GetEnumValueTest(System.Int32)")]
+#endif
