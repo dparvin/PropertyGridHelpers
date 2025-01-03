4d73801f2f4bb34d848c5339ce299b0944510645
David Parvin <parvin_david@hotmail.com>
2025-01-02 21:50:16 -0700

    moved the editor config and coverlet.runsettings

--- Code/SampleControls/GlobalSuppressions.cs
+++ Code/SampleControls/GlobalSuppressions.cs
@@ -5,10 +5,6 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Performance", "CA1859:Use concrete types when possible for improved performance", Justification = "<Pending>", Scope = "member", Target = "~M:SampleControls.TestControl.GetImage(SampleControls.ImageTypes)~System.Drawing.Image")]
-[assembly: SuppressMessage("Style", "IDE0008:Use explicit type", Justification = "<Pending>", Scope = "member", Target = "~M:SampleControls.TestControl.GetImage(SampleControls.ImageTypes)~System.Drawing.Image")]
-[assembly: SuppressMessage("Globalization", "CA1304:Specify CultureInfo", Justification = "<Pending>", Scope = "member", Target = "~M:SampleControls.TestControl.GetImage(SampleControls.ImageTypes)~System.Drawing.Image")]
-[assembly: SuppressMessage("Usage", "CA2263:Prefer generic overload when type is known", Justification = "<Pending>", Scope = "member", Target = "~M:SampleControls.TestControl.GetImage(SampleControls.ImageTypes)~System.Drawing.Image")]
 [assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "<Pending>", Scope = "member", Target = "~P:SampleControls.TestControl.Strings")]
 [assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "<Pending>", Scope = "member", Target = "~P:SampleControls.TestControl.Decimals")]
 [assembly: SuppressMessage("Style", "IDE0028:Simplify collection initialization", Justification = "<Pending>", Scope = "member", Target = "~P:SampleControls.TestControl.Sizes")]
