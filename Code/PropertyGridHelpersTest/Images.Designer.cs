dc4023b657594251e632a9d3692c27503abe764e
David Parvin <parvin_david@hotmail.com>
2025-02-15 19:09:43 -0700

    Added Resourced to one of the resource files

--- Code/PropertyGridHelpersTest/Images.Designer.cs
+++ Code/PropertyGridHelpersTest/Images.Designer.cs
@@ -8,8 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PropertyGridHelpersTest
-{
+namespace PropertyGridHelpersTest {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -20,107 +22,89 @@ namespace PropertyGridHelpersTest
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal partial class Images
-    {
-
+    internal class Images {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Images()
-        {
+        internal Images() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if (object.ReferenceEquals(resourceMan, null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PropertyGridHelpersTest.Images", typeof(Images).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        internal static byte[] confetti
-        {
-            get
-            {
+        internal static byte[] confetti {
+            get {
                 object obj = ResourceManager.GetObject("confetti", resourceCulture);
                 return ((byte[])(obj));
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        internal static byte[] confetti_jpg
-        {
-            get
-            {
+        internal static byte[] confetti_jpg {
+            get {
                 object obj = ResourceManager.GetObject("confetti.jpg", resourceCulture);
                 return ((byte[])(obj));
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap confetti_stars
-        {
-            get
-            {
+        internal static System.Drawing.Bitmap confetti_stars {
+            get {
                 object obj = ResourceManager.GetObject("confetti-stars", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap confetti_stars_bitmap
-        {
-            get
-            {
+        internal static System.Drawing.Bitmap confetti_stars_bitmap {
+            get {
                 object obj = ResourceManager.GetObject("confetti-stars-bitmap", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to Stars.
         /// </summary>
-        internal static string Stars
-        {
-            get
-            {
+        internal static string Stars {
+            get {
                 return ResourceManager.GetString("Stars", resourceCulture);
             }
         }
